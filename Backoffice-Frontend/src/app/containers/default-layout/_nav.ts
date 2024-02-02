ae1820ce53e360897bf7f0acb3ba3835e64e33a2
MichaelRandriamizara <mkrandriamizara@gmail.com>
2024-02-02 15:55:21 +0300

    init front-end back office

---
+++ Backoffice-Frontend/src/app/containers/default-layout/_nav.ts
@@ -0,0 +1,10 @@
+import { INavData } from '@coreui/angular';
+
+export const navItems: INavData[] = [
+  {
+    name: 'Tableau de bord',
+    url: '/dashboard',
+    iconComponent: { name: 'cil-speedometer' },
+  },
+
+];
