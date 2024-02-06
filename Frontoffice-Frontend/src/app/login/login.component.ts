90af6fa01862166600261b42d86a4d9c935b0661
MichaelRandriamizara <mkrandriamizara@gmail.com>
2024-02-06 13:33:36 +0300

    login user

--- Frontoffice-Frontend/src/app/login/login.component.ts
+++ Frontoffice-Frontend/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../services/auth.service';
+import {StorageService} from '../services/storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   focus;
   focus1;
-  constructor() { }
+  form: any = {
+    email: null,
+    password: null
+  };
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  // roles: string[] = [];
+  constructor(private authService: AuthService, private storageService: StorageService,private router:Router) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    if (this.storageService.isLoggedIn()) {
+      this.isLoggedIn = true;
+      // this.roles = this.storageService.getUser().roles;
+    }
+  }
+
+  onSubmit(): void {
+    const { email, password } = this.form;
+    console.log(email, password);
+    this.authService.login(email, password).subscribe({
+      next: data => {
+        this.storageService.saveUser(data);
+
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        // this.roles = this.storageService.getUser().roles;
+        // this.reloadPage();
+        this.router.navigate(['/home']);
+      },
+      error: err => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      }
+    });
+  }
+
+  reloadPage(): void {
+    window.location.reload();
   }
 
 }
---
+++ Frontoffice-Frontend/src/configurations/server.config.ts
@@ -0,0 +1,4 @@
+// import config from './server.dev.json';
+
+export const baseUrl = (url: string) => 'http://localhost:3000/'.concat(url);
+
