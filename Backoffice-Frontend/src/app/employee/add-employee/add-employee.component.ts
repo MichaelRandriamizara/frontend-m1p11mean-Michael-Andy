71bd7add114831c1cdce763595bf0283fd1d54a5
AndyNyRindra <92746662+AndyNyRindra@users.noreply.github.com>
2024-02-10 19:28:32 +0300

    upload image on create employee

--- Backoffice-Frontend/src/app/employee/add-employee/add-employee.component.ts
+++ Backoffice-Frontend/src/app/employee/add-employee/add-employee.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {EmployeeService} from "../../services/employee/employee.service";
 import {RoleService} from "../../services/role/role.service";
+import {getBase64} from "../../utils/upload-image.util";
 
 @Component({
   selector: 'app-add-employee',
@@ -15,12 +16,19 @@ export class AddEmployeeComponent implements OnInit{
     email: null,
     password: "1234",
     phone: null,
-    photo: "aaaa",
+    photo: null,
     role: null
   };
 
   roles: any = [];
 
+  dragAreaClass: string = "dragarea";
+  onFileChange(event: any) {
+    let files: FileList = event.target.files;
+    getBase64(files[0]).then((base64) => {
+      this.form.photo = base64;
+    });
+  }
 
   constructor( private employeeService: EmployeeService, private roleService: RoleService, private router: Router) {
   }
@@ -37,5 +45,35 @@ export class AddEmployeeComponent implements OnInit{
     this.roleService.getAll(data => {
       this.roles = data;
     });
+    this.dragAreaClass = "dragarea";
+  }
+
+  @HostListener("dragover", ["$event"]) onDragOver(event: any) {
+    this.dragAreaClass = "droparea";
+    event.preventDefault();
+  }
+  @HostListener("dragenter", ["$event"]) onDragEnter(event: any) {
+    this.dragAreaClass = "droparea";
+    event.preventDefault();
+  }
+  @HostListener("dragend", ["$event"]) onDragEnd(event: any) {
+    this.dragAreaClass = "dragarea";
+    event.preventDefault();
+  }
+  @HostListener("dragleave", ["$event"]) onDragLeave(event: any) {
+    this.dragAreaClass = "dragarea";
+    event.preventDefault();
+  }
+  @HostListener("drop", ["$event"]) onDrop(event: any) {
+    this.dragAreaClass = "dragarea";
+    event.preventDefault();
+    event.stopPropagation();
+    if (event.dataTransfer.files) {
+      let files: FileList = event.dataTransfer.files;
+      getBase64(files[0]).then((base64) => {
+        this.form.photo = base64;
+      });
+
+    }
   }
 }
