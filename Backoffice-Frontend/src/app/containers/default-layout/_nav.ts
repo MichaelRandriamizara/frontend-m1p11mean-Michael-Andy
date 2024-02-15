e9ee6bb7781afc70edcbb9fd18285b6fec270adf
MichaelRandriamizara <mkrandriamizara@gmail.com>
2024-02-15 10:33:06 +0300

    remove from navbar when unauthorized page

--- Backoffice-Frontend/src/app/containers/default-layout/_nav.ts
+++ Backoffice-Frontend/src/app/containers/default-layout/_nav.ts
@@ -1,24 +1,33 @@
 import { INavData } from '@coreui/angular';
+import {AUTH} from "../../auth/auth";
 
-export const navItems: INavData[] = [
+export interface CustomNavData extends INavData {
+  auth?: number;
+}
+
+export const navItems: CustomNavData[] = [
   {
     name: 'Tableau de bord',
     url: '/dashboard',
+    auth: AUTH.EMPLOYEE.value,
     iconComponent: { name: 'cil-speedometer' },
   },
   {
     name: 'Service',
     url: '/service',
+    auth: AUTH.ADMIN.value,
     iconComponent: { name: 'cil-star' },
   },
   {
     name: 'Employé',
     url: '/employe',
+    auth: AUTH.ADMIN.value,
     iconComponent: { name: 'cil-people' },
   },
   {
     name: 'Type de dépense',
     url: '/type-depense',
+    auth: AUTH.ADMIN.value,
     iconComponent: { name: 'cil-drop' },
   }
 
