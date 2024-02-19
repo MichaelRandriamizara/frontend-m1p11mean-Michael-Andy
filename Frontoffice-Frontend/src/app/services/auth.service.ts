e7a8470b243012fa3678718f80dc61df5c8c8094
MichaelRandriamizara <mkrandriamizara@gmail.com>
2024-02-19 13:32:17 +0300

    fix ne peut pas noter apres inscription

--- Frontoffice-Frontend/src/app/services/auth.service.ts
+++ Frontoffice-Frontend/src/app/services/auth.service.ts
@@ -46,16 +46,22 @@ export class AuthService {
 
     }
 
-    register(name: string, email: string, password: string): Observable<any> {
-        return this.http.post(
-            AUTH_API + 'signup',
-            {
-                name,
-                email,
-                password,
-            },
-            httpOptions
-        );
+    register(name: string, email: string, password: string, next: any) {
+        startApiCall(close => {
+            return this.http.post(
+                AUTH_API + 'signup',
+                {
+                    name,
+                    email,
+                    password,
+                },
+                httpOptions
+            ).subscribe(ObserverObject((data) => {
+                this.storageService.saveUser(data);
+                next(data);
+                close();
+            }));
+        });
     }
 
     logout(): Observable<any> {
