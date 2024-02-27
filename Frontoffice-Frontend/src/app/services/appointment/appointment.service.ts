58261a66b8331e06e47ad1e97ef68ee056fe1910
AndyNyRindra <92746662+AndyNyRindra@users.noreply.github.com>
2024-02-27 22:54:14 +0300

    prise rdv (sans paiement)

--- Frontoffice-Frontend/src/app/services/appointment/appointment.service.ts
+++ Frontoffice-Frontend/src/app/services/appointment/appointment.service.ts
@@ -1,23 +1,26 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import {baseUrl} from '../../../configurations/server.config';
 import {startApiCall} from '../../utils/sweet-alert.utils';
 import {ObserverObject} from '../../utils/error.handler';
+import {StorageService} from '../storage.service';
 
 const APPOINTMENT_API = baseUrl('api/tasks/appointment/');
-
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppointmentService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private storageService: StorageService) { }
 
   makeAppointment(data: any, next: (res: any) => void) {
+      const httpOptions = {
+          headers: new HttpHeaders({
+              'Content-Type': 'application/json',
+              'userid': this.storageService.getUser().id,
+          }),
+      };
     startApiCall(close => {
         this.http.post(APPOINTMENT_API, data, httpOptions).subscribe(res => {
             next(res);
@@ -26,9 +29,18 @@ export class AppointmentService {
     });
   }
 
-  createAppointment(data: any, next: () => void) {
-    this.http.post(APPOINTMENT_API + 'confirm', data, httpOptions).subscribe(res => {
-      next();
-    });
+  createAppointment(data: any, next: (res: any) => void) {
+      const httpOptions = {
+          headers: new HttpHeaders({
+              'Content-Type': 'application/json',
+              'userid': this.storageService.getUser().id,
+          }),
+      };
+      startApiCall(close => {
+          this.http.post(APPOINTMENT_API + 'confirm', data, httpOptions).subscribe(res => {
+              next(res);
+              close();
+          });
+      });
   }
 }
