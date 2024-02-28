6693f9a5f7658e2543f5d787c2e04b2ba39f45cb
MichaelRandriamizara <mkrandriamizara@gmail.com>
2024-02-28 21:12:37 +0300

    change baseUrl

--- Backoffice-Frontend/src/app/configurations/server.config.ts
+++ Backoffice-Frontend/src/app/configurations/server.config.ts
@@ -1,4 +1,5 @@
 // import config from './server.dev.json';
 
-export const baseUrl = (url: string) => 'http://localhost:3000/'.concat(url);
+// export const baseUrl = (url: string) => 'http://localhost:3000/'.concat(url);
+export const baseUrl = (url: string) => 'https://backend-m1p11mean-michael-andy.onrender.com/'.concat(url);
 
